3c6297124159f8986f90b0a0ebe792f2c80b0ce4
Nantomahmicah <nantomahmicah@gmail.com>
2026-09-12 14:26:28 +0100

    add marketplce

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "../app/Lib/supabase";
+import { supabase } from "./Lib/supabase";
 import type { User } from "@supabase/supabase-js";
-import PayoutSetup from "../app/components/PayoutSetup";
-import SellerEarnings from "../app/components/SellerEarnings";
-import FoodOptionsManager from "../app/components/OptionsManager";
+import PayoutSetup from "./components/PayoutSetup";
+import SellerEarnings from "./components/SellerEarnings";
+import FoodOptionsManager from "./components/OptionsManager";
 
 type Seller = {
   id: number;
